6e66a37b59b3f7bfa82cef6ad2439b04923a30b4
jay-all-access-io <jay@all-access.io>
2024-12-04 16:31:46 +0800

    Init

---
+++ components/button/Button.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { type ButtonVariants, buttonVariants } from '.'
+import { cn } from '@/lib/utils'
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="loading"
+  >
+    <i v-if="loading" class="i-lucide-loader-circle mr-2 size-4 animate-spin" />
+    <slot />
+  </Primitive>
+</template>
+
+<style>
+</style>
